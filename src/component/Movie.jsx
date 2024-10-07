16ebfaf99c83632229cafef14e9a2ba05f3a3a28
Ladan-Mirzaei <161485911+Ladan-Mirzaei@users.noreply.github.com>
2024-10-07 12:04:53 +0200

    neu

--- src/component/Movie.jsx
+++ src/component/Movie.jsx
@@ -1,14 +1,14 @@
 import loginImage from "../assets/avatar.jpg";
 
 function Movie({ movie, movieToDelete, movieAddFavorite }) {
-  const imagePath = `/assets/${movie.title
+  const imagePath = `./assets/${movie.title
     .toLowerCase()
     .replace(/\s+/g, "_")}.jpg`;
 
   console.log(imagePath);
   return (
     <div className="movie2">
-      <img src={loginImage} alt={movie.title} /> <h2>{movie.title}</h2>
+      <img src={imagePath} alt={movie.title} /> <h2>{movie.title}</h2>
       <p>Released: {movie.released}</p>
       <p>Director: {movie.director}</p>
       <div className="button-container">
